e553712a329934f68db3fce031c3a844f895bdc1
Artem <bolotskiu2006@gmail.com>
2025-07-20 17:54:27 +0700

    Add files via upload

--- diploma-project/js/main.js
+++ diploma-project/js/main.js
@@ -41,15 +41,14 @@ if (counters) {
             if (target.closest(".quantity__counter-button")) {
                 let value = parseInt(target.closest(".quantity").querySelector('input').value)
 
-                console.log(target.classList.contains('quantity__counter-button--plus'))
                 if (target.classList.contains('quantity__counter-button--plus') || target.classList.contains('quantity__plus') || target.classList.contains('quantity__plus-svg') || target.classList.contains('quantity__plus-path')) {
                     value++
                 } else {
                     --value
                 }
 
-                if (value <= 0) {
-                    value = 0
+                if (value <= 1) {
+                    value = 1
                     target.closest(".quantity").querySelector('.quantity__counter-button--minus').classList.add('disabled')
                 } else {
                     target.closest(".quantity").querySelector('.quantity__counter-button--minus').classList.remove('disabled')
@@ -64,28 +63,15 @@ if (counters) {
 // icon-fav
 
 document.querySelectorAll('.favorite-btn').forEach(btn => {
-    btn.innerHTML = `<svg class="heart-icon-svg" width="52" height="52" viewBox="0 0 37 37" fill="none"
-                                    xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                                    <desc>
-                                        Created with Pixso.
-                                    </desc>
-                                    <defs />
-                                    <rect id="избранное (хедер)" rx="0.000000" width="36.000000" height="36.000000"
-                                        transform="translate(0.500000 0.500000)" fill="#FFFFFF" fill-opacity="0" />
-                                    <path id="Vector"
-                                        d="M5.64 21.98C2.22 18.42 2.44 12.59 6.11 9.31C9.75 6.06 15.27 6.7 18.14 10.69L18.63 11.36L19.11 10.69C21.99 6.7 27.51 6.06 31.15 9.31C34.82 12.59 35.03 18.42 31.62 21.98L19.48 34.61C19.01 35.11 18.25 35.11 17.77 34.61L5.64 21.98Z"
-                                        stroke="#FFFFFF" stroke-opacity="1.000000" stroke-width="2.000000"
-                                        stroke-linejoin="round" />
-                                </svg>`;
-
-    btn.addEventListener('click', function () {
+    btn.addEventListener('click', function (event) {
+        event.preventDefault()
         this.classList.toggle('active');
     });
 });
 
+//swiper-product-card
 
-
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.product-gallery-slider', {
     // Параметры
     direction: 'vertical', // или 'vertical'
     slidesPerView: 5,
@@ -103,3 +89,309 @@ const swiper = new Swiper('.swiper', {
         el: '.swiper-scrollbar',
     },
 });
+
+//btn-readMore
+
+const dots = document.getElementById("dots")
+const more = document.getElementById("more")
+const btn = document.getElementById("product-button-more")
+
+function toggleDescription() {
+    if (dots.style.display === 'none') {
+        dots.style.display = 'inline'
+        btn.innerHTML = 'Показать описание'
+        more.style.display = 'none'
+    } else {
+        dots.style.display = 'none'
+        btn.innerHTML = 'Скрыть'
+        more.style.display = 'inline'
+    }
+}
+
+function handleResize() {
+    if (window.innerWidth > 900) {
+        dots.style.display = '';
+        more.style.display = '';
+        btn.innerHTML = 'Показать описание';
+        btn.style.display = 'none';
+    } else {
+        btn.style.display = '';
+    }
+}
+
+btn.addEventListener('click', function (event) {
+    event.preventDefault();
+    toggleDescription();
+});
+
+window.addEventListener('resize', handleResize);
+
+handleResize();
+
+
+// tabs
+
+const tabControls = document.querySelector('.tab-controls')
+
+tabControls.addEventListener('click', toggleTab)
+
+function toggleTab(e) {
+
+    const tabControl = e.target.closest('.tab-controls__link')
+    const tabControlItem = tabControl.closest('.tab-controls__item')
+
+    if (!tabControl || !tabControlItem) return
+    e.preventDefault()
+    if (tabControl.classList.contains('tab-controls__link--active') || tabControlItem.classList.contains('tab-controls__item--active')) return
+
+    const tabContentID = tabControl.getAttribute('href')
+    const tabContent = document.querySelector(tabContentID)
+    const activeControl = document.querySelector('.tab-controls__link--active')
+    const activeItem = document.querySelector('.tab-controls__item--active')
+    const activeContent = document.querySelector('.tab-content--show')
+
+    if (activeControl) {
+        activeControl.classList.remove('tab-controls__link--active')
+    }
+    if (activeItem) {
+        activeItem.classList.remove('tab-controls__item--active')
+    }
+
+    if (activeContent) {
+        activeContent.classList.remove('tab-content--show')
+    }
+
+    tabControl.classList.add('tab-controls__link--active')
+    tabControlItem.classList.add('tab-controls__item--active')
+    tabContent.classList.add('tab-content--show')
+}
+
+
+// form
+
+const btnDelivery = document.querySelector('.button-delivery')
+
+btnDelivery.addEventListener('click', function (e) {
+    e.preventDefault()
+})
+
+document.getElementById('city').addEventListener('change', function () {
+    const otherCityContainer = document.getElementById('otherCityContainer');
+    otherCityContainer.style.display = this.value === 'other' ? 'block' : 'none';
+    if (this.value !== 'other') {
+        document.getElementById('otherCity').value = '';
+    }
+});
+
+
+
+// Аккардеон
+
+const accordionLists = document.querySelectorAll('.accordion-list')
+
+
+accordionLists.forEach(el => {
+
+    // реализация изначально открытой первой вкладки аккардиона
+    // document.querySelector('.accordion-list__item--opened .accordion-list__content').style.maxHeight = document.querySelector('.accordion-list__item--opened .accordion-list__content').scrollHeight + 'px'
+
+    el.addEventListener('click', (e) => {
+
+        const accordionList = e.currentTarget
+        const accordionOpenedItem = accordionList.querySelector('.accordion-list__item--opened')
+        const accordionOpenedContent = accordionList.querySelector('.accordion-list__item--opened .accordion-list__content')
+
+        const accordionControl = e.target.closest('.accordion-list__control')
+        if (!accordionControl) return
+        const accordionItem = accordionControl.parentElement
+        const accordionContent = accordionControl.nextElementSibling
+        e.preventDefault()
+        if (accordionOpenedItem && accordionItem != accordionOpenedItem) {
+            accordionOpenedItem.classList.remove('accordion-list__item--opened')
+            accordionOpenedContent.style.maxHeight = null
+        }
+
+        accordionItem.classList.toggle('accordion-list__item--opened')
+
+        if (accordionItem.classList.contains('accordion-list__item--opened')) {
+            accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px'
+
+        } else {
+            accordionContent.style.maxHeight = null
+        }
+
+    })
+})
+
+
+
+
+// btn-more
+
+const btnMore = document.querySelector('.similar-products__btn');
+const cards = Array.from(document.querySelectorAll('.similar-products__card'));
+
+
+window.addEventListener('resize', event => {
+    if (event.target.window.innerWidth > 1499) response0();
+    if (event.target.window.innerWidth <= 1499 && event.target.window.innerWidth > 900) response1();
+    if (event.target.window.innerWidth <= 900 && event.target.window.innerWidth > 763) response2();
+    if (event.target.window.innerWidth <= 763 && event.target.window.innerWidth > 600) response3();
+    if (event.target.window.innerWidth <= 600 && event.target.window.innerWidth > 389) response4();
+    if (event.target.window.innerWidth <= 389) response5();
+})
+
+function openCatalog() {
+    btnMore.addEventListener('click', () => {
+        cards.forEach(item => item.classList.remove('hidden'));
+        btnMore.classList.add('hidden');
+    })
+}
+
+function response0() {
+    if (window.innerWidth > 1499) {
+
+        cards.forEach((item, index) => {
+            item.classList.add('hidden')
+            if (index <= 2) {
+                item.classList.remove('hidden')
+            } else if (index > 2) {
+                btnMore.classList.remove('hidden');
+            }
+            openCatalog()
+        })
+    }
+}
+response0()
+
+function response1() {
+    if (window.innerWidth <= 1499 && window.innerWidth > 900) {
+
+        cards.forEach((item, index) => {
+            item.classList.add('hidden')
+            if (index <= 3) {
+                item.classList.remove('hidden')
+            } else if (index > 3) {
+                btnMore.classList.remove('hidden');
+            }
+            openCatalog()
+        })
+    }
+}
+response1()
+
+
+function response2() {
+    if (window.innerWidth <= 900 && window.innerWidth > 763) {
+
+        cards.forEach((item, index) => {
+            item.classList.add('hidden')
+            if (index <= 5) {
+                item.classList.remove('hidden')
+            } else if (index > 5) {
+                btnMore.classList.remove('hidden');
+            }
+            openCatalog()
+        })
+    }
+}
+response2()
+
+
+function response3() {
+    if (window.innerWidth <= 763 && window.innerWidth > 600) {
+
+        cards.forEach((item, index) => {
+            item.classList.add('hidden')
+            if (index <= 3) {
+                item.classList.remove('hidden')
+            } else if (index > 3) {
+                btnMore.classList.remove('hidden');
+            }
+            openCatalog()
+        })
+    }
+}
+response3()
+
+function response4() {
+    if (window.innerWidth <= 600 && window.innerWidth > 389) {
+
+        cards.forEach((item, index) => {
+            item.classList.add('hidden')
+            if (index <= 3) {
+                item.classList.remove('hidden')
+            } else if (index > 3) {
+                btnMore.classList.remove('hidden');
+            }
+            openCatalog()
+        })
+    }
+}
+response4()
+
+function response5() {
+    if (window.innerWidth <= 389) {
+
+        cards.forEach((item, index) => {
+            item.classList.add('hidden')
+            if (index <= 1) {
+                item.classList.remove('hidden')
+            } else if (index > 1) {
+                btnMore.classList.remove('hidden');
+            }
+            openCatalog()
+        })
+    }
+}
+response5()
+
+// swiper for one
+
+const swiperOneImg = new Swiper('.product-gallery-active-list', {
+    // Параметры
+    direction: 'horizontal', // или 'vertical'
+    slidesPerView: 5,
+    spaceBetween: 20,
+    loop: false, // бесконечный слайдер
+    pagination: {
+        el: '.product-gallery-active-list-pagination',
+        clickable: true,
+    },
+});
+
+const activeList = document.querySelector('.product-gallery-active-list')
+const activeListSwiper = activeList.querySelector('.gallery-active-slider')
+const activeItems = activeList.querySelectorAll('.product-gallery-active-item')
+
+window.addEventListener('resize', winWidth);
+
+function winWidth() {
+    if (window.innerWidth <= 625) {
+        activeList.classList.add('swiper')
+        activeListSwiper.classList.add('swiper-wrapper')
+        for (let elem of activeItems) {
+            elem.classList.add('swiper-slide')
+        }
+    }
+    if (window.innerWidth > 625 && activeList.closest('.swiper')) {
+        activeList.classList.remove('swiper')
+        activeListSwiper.classList.remove('swiper-wrapper')
+        for (let elem of activeItems) {
+            elem.classList.remove('swiper-slide')
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
